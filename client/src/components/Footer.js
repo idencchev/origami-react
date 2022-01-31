42cf159dec4698a18917e65a68cda9dbc51c7c80
idenccev <idenchev@gmail.com>
2022-01-31 22:34:09 +0200

    jjj

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,17 +9,20 @@ class Footer extends Component {
 
         return (
             <footer className={this.props.style.footer}>
-                <li className={this.props.style.listItem}><a href="#">Going to 1</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 2</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 3</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 4</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 5</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 6</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 7</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 8</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 9</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 10</a></li>
-                <li className={this.props.style.listItem}><a href="#">Going to 11</a></li>
+                <ul>
+                    <li className={this.props.style.listItem}><a href="#">Going to 1</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 2</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 3</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 4</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 5</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 6</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 7</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 8</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 9</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 10</a></li>
+                    <li className={this.props.style.listItem}><a href="#">Going to 11</a></li>
+                </ul>
+                <p>Software University &copy; 2022</p>
             </footer>
         )
     }
